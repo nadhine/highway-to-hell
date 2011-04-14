08028661e60166ab1d3cc8b5dbcb19a083a55edb
hangrel <hangrel@44eeaf71-29e8-4c5f-eb3d-5d2be3c1ec2d>
2011-04-14 16:01:29 +0000

    classe jogador

---
+++ trunk/jogador.py
@@ -0,0 +1,17 @@
+from carro import *
+
+class Jogador():
+    def __init__(self,nome):
+        self.nome = nome
+        self.carro = Carro()
+    def getNome(self):
+        return sel.nome
+    def setNome(self,nome):
+        if len(nome) > 1 and len(nome) <= 10:
+            self.nome = nome
+        else:
+            return
+    def getCarro(self):
+        return self.carro
+    def setCarro(self,carro):
+        self.carro = carro
